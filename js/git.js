e4d8e1ba3728de81d3638a336f7f91562ab9afc7
Roopa Hiremath Chandrasekaraiah <roopaniharika@gmail.com>
2015-01-26 01:09:35 -0700

    Issues 1,2,3,4,6,7 fixed

---
+++ js/git.js
@@ -0,0 +1,18 @@
+ 	 
+    <script>
+     
+function hide( t,b1,b2) {
+ 
+document.getElementById(t).style.display = "none";
+document.getElementById(b2).style.display="none"
+document.getElementById(b1).style.display="inline"
+}
+
+function show( t,b1,b2) {
+
+document.getElementById(t).style.display = "block";
+document.getElementById(b1).style.display="none"
+document.getElementById(b2).style.display="inline"
+}
+
+    </script>
